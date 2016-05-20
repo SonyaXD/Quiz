3c252f2634e599f37c78ae8c4f0fc65dfdbeae5f
Sonia Gil <sogiga25@gmail.com>
2016-05-20 14:11:08 +0200

    Tema 10: Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -28,11 +28,14 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync()
-.then(function() { // sync() crea la tabla quiz
+.then(function() { // Ya se han creado las tablas necesarias
 	return Quiz.count()
 	  .then(function (c) {
-	  	if (c === 0) { // la tabla se inicializa si está vacía
-	  	  return Quiz.create({ question: 'Capital de Italia', answer: 'Roma' })
+	  	if (c === 0) { // la tabla se inicializa solo si está vacía
+	  	  return Quiz
+	  	         .bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
+	  	  	                   {question: 'Capital de Portugal', answer: 'Lisboa'}
+	  	  	                 ])  
 	  	    .then(function() {
 	  	    	console.log('Base de datos inicializada con datos');
 	  	    });
