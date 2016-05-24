dd28457bc39435ecc0579e5dba1c2b309b549a26
Sonia Gil <sogiga25@gmail.com>
2016-05-24 18:10:31 +0200

    Tema 11: Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,5 @@
 var models = require('../models');
 
-
 //Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId)
@@ -15,25 +14,28 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizzes
-exports.index = function(req,res, next) {
+exports.index = function(req, res, next) {
 	models.Quiz.findAll()
-	.then(function(quizzes) {
-		res.render('quizzes/index.ejs', { quizzes: quizzes});
-	}).catch(function(error) { next(error); });
+		.then(function(quizzes) {
+			res.render('quizzes/index.ejs', { quizzes: quizzes});
+		})
+		.catch(function(error) { next(error); });
 };
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
-	var answer = req.query.answer || '';
-	res.render('quizzes/show', {quiz: req.quiz,
-                                answer: answer});
+
+				var answer = req.query.answer || '';
+				res.render('quizzes/show', {quiz: req.quiz,
+                                            answer: answer});
 };
 
 // GET /quizzes/:id/check
 exports.check = function(req, res, next) {
-	var answer = req.query.answer || "";
-	var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
-		res.render('quizzes/result', { quiz: req.quiz, 
-			                           result: result, 
-			                           answer: answer });
+
+			var answer = req.query.answer || "";
+			var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+			res.render('quizzes/result', { quiz: req.quiz, 
+			                               result: result, 
+			                               answer: answer });
 };
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res,next) {
 });
 
 //Autoload de rutas que usen :quizId
-router.param('quizId', quizController.load);  //autoload :quizId
+router.param('quizId', quizController.load); //autoload :quizId
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                          quizController.index);
