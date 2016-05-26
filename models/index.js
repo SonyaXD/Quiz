7e50f96c4313eb8899cd04ce79e5bb8158813eb6
Sonia Gil <sogiga25@gmail.com>
2016-05-26 13:17:47 +0200

    Despliegue Tema 12 Heroku

--- models/index.js
+++ models/index.js
@@ -33,8 +33,8 @@ sequelize.sync()
 	  .then(function (c) {
 	  	if (c === 0) { // la tabla se inicializa solo si está vacía
 	  	  return Quiz.bulkCreate([ {question: 'Capital de Italia', answer: 'Roma'},
-	  	  	                   {question: 'Capital de Portugal', answer: 'Lisboa'}
-	  	  	                 ])  
+	  	  	                       {question: 'Capital de Portugal', answer: 'Lisboa'}
+	  	  	                     ])  
 	  	    .then(function() {
 	  	    	console.log('Base de datos inicializada con datos');
 	  	    });
