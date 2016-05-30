a2de9e460f0f81658b253a64fe3286dab8b1122f
Sonia Gil <sogiga25@gmail.com>
2016-05-30 13:32:09 +0200

    Tema 20 (error msg)

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,26 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res,next) {
   res.render('index', { title: 'Quiz'});
 });
 
-//Autoload de rutas que usen :quizId
+//Autoload de parametros
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('userId', userController.load); //autoload :userId
+
+// Definicion de rutas de cuenta
+router.get('/users',                            userController.index); //Listado usuarios
+router.get('/users/:userId(\\d+)',              userController.show); //Ver un usuario
+router.get('/users/new',                        userController.new); //Formulario sign in
+router.post('/users',                           userController.create); //Registrar usuario
+router.get('/users/:userId(\\d+)/edit',         userController.edit); //Editar cuenta
+router.put('/users/:userId(\\d+)',              userController.update); //Actualizar cuenta
+router.delete('/users/:userId(\\d+)',          userController.destroy); //Borrar cuenta
+
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                          quizController.index);
